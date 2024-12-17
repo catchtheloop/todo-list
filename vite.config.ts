63f0c31b5f3555ddb636137939e1beb6db4f1eca
sorazaz <sebgarrido@gmail.com>
2024-12-17 17:58:28 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-app/",
+  base: "/todo-list/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
